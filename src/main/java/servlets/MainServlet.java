7d750d684429ccdeb0bf9d1c68a293e4dbad9b4f
SanyaTW <p_a.s.nosach@mpt.ru>
2019-09-23 16:45:04 +0300

    Отправка сообщений

--- src/main/java/servlets/MainServlet.java
+++ src/main/java/servlets/MainServlet.java
@@ -11,7 +11,7 @@ public class MainServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        req.getRequestDispatcher("/views/Home.jsp").forward(req, resp);
     }
 
     @Override
