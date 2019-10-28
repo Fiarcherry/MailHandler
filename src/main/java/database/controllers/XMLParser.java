727364609b50976bb9657dc3d67879af30a6e95e
Fiarcherry <fiarchersschannel@gmail.com>
2019-10-28 16:28:20 +0300

    Следит за структурой XML-файлов

--- src/main/java/database/controllers/XMLParser.java
+++ src/main/java/database/controllers/XMLParser.java
@@ -1,5 +1,6 @@
 package database.controllers;
 
+import database.models.ErrorM;
 import database.models.PaymentM;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -9,10 +10,12 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Date;
 
 public class XMLParser {
 
-    public static PaymentM parsePayment(File savefile){
+    public static PaymentM parsePayment(File savefile) throws SQLException {
 
         try {
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
@@ -24,16 +27,19 @@ public class XMLParser {
             Element root = doc.getDocumentElement();
 
             return new PaymentM(
-                    root.getElementsByTagName("Uni").item(0).getTextContent(),
-                    root.getElementsByTagName("Number").item(0).getTextContent(),
-                    root.getElementsByTagName("DateOperation").item(0).getTextContent(),
-                    Float.valueOf(root.getElementsByTagName("Amount").item(0).getTextContent()),
-                    Float.valueOf(root.getElementsByTagName("Commission").item(0).getTextContent()),
+                        root.getElementsByTagName("Id").item(0).getTextContent(),
+                        root.getElementsByTagName("IdOrder").item(0).getTextContent(),
+                        root.getElementsByTagName("DatePayment").item(0).getTextContent(),
+                        Float.valueOf(root.getElementsByTagName("AmountPayment").item(0).getTextContent()),
+                        Float.valueOf(root.getElementsByTagName("CommissionPayment").item(0).getTextContent()),
                     false);
         }
         catch (ParserConfigurationException | IOException | org.xml.sax.SAXException e){
             e.printStackTrace();
+            return null;
         }
-        return null;
+        catch(NullPointerException e){
+         return null;
+        }
     }
 }
